ea8574f2cfa903344517de296baab7877f8c5180
Emmanuel Aderibigbe <aderibigbeemmanuel2004@gmail.com>
2025-08-19 04:48:59 +0100

    issues fixed

--- src/pages/home/components/nav/mobile-nav/mobileNav.tsx
+++ src/pages/home/components/nav/mobile-nav/mobileNav.tsx
@@ -1,29 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { CloseOutlined, DownOutlined } from '@ant-design/icons';
 
-import {  useNavigate } from 'react-router-dom';
-import { UserContext } from '../../../../../utils/context/user/UserContext';
-import { UserProps } from '../../../../../utils/context/user/types/UserType';
+import { useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../../../../utils/context/store/StoreContext';
 import { StoreProps } from '../../../../../utils/context/store/StoreProps';
-import { logOutUser } from '../../../../../utils/firebase/auth/firebaseAuth';
 
 const MobileNav = () => {
 	const navigate = useNavigate();
 
-	const { user } = useContext(UserContext) as UserProps;
 	const { setMobileNav } = useContext(StoreContext) as StoreProps;
+	const [openMenu, setOpenMenu] = useState<string | null>(null);
 
+	const toggleMenu = (menu: string) => {
+		setOpenMenu(openMenu === menu ? null : menu);
+	};
 	return (
-		<div className=' z-40 fixed w-screen top-0 left-0 h-screen  flex'>
+		<div className=' z-40 fixed w-screen font-poppins top-0 left-0 h-screen  flex'>
 			<div className=' w-[90%] md:w-[40%] opacity-100 px-5 py-5 z-40 bg-white relative h-full ml-auto overflow-scroll '>
 				<div className=' '>
-					<div className='flex font-serrat gap-6 flex-col '>
+					<div className='flex font-poppins gap-6 flex-col '>
 						<div className=' flex '>
 							<h1
-								className='text-main font-serrat font-bold text-2xl md:text-3xl cursor-pointer'
+								className='text-main font-poppins font-bold text-2xl md:text-3xl cursor-pointer'
 								onClick={() => navigate('/')}>
-								Luxenexus
+								BeadsbyTricia
 							</h1>
 
 							<CloseOutlined
@@ -31,173 +31,176 @@ const MobileNav = () => {
 								onClick={() => setMobileNav(false)}
 							/>
 						</div>
-						<ul className='flex flex-col text-main  gap-5 text-sm'>
-							<li className='relative group'>
-								<h1  className='underline-custom'>
-									What's new
-								</h1>
-								{/* Hover Dropdown */}
-								<div className='absolute z-30 hidden cursor-pointer group-hover:flex flex-col mt-0 w-[200px]  font-serrat gap-5 top-full left-0 bg-white p-4 shadow-lg transition-all duration-200'>
-									<p onClick={() => {
-										navigate('/new-arrival')
-										setMobileNav(false)
-									 }}>New Arrivals</p>
-									<p onClick={() => {
-										navigate('/trending')
-										setMobileNav(false)
-									 }}>Trending</p>
-								</div>
-							</li>
 
-							<li className='relative group'>
-								<h1  className='underline-custom'>
-									Shop
-								</h1>
-								<div className='absolute z-30 hidden cursor-pointer group-hover:flex flex-col mt-0 w-[200px]  font-serrat gap-5 top-full left-0 bg-white p-4 shadow-lg transition-all duration-200'>
-									<p
-										onClick={() => {
-											navigate('/shop');
-											setMobileNav(false);
-										}}>
-										All Products
-									</p>
-									<p
-										onClick={() => {
-											navigate('/popular-items');
-											setMobileNav(false);
-										}}>
-										Popular Items
-									</p>
-								</div>
-							</li>
-
-							<li className='relative group'>
-								<h1
-									onClick={() => {
-										navigate('/clothing');
-										setMobileNav(false);
-									}}
-									className='underline-custom'>
-									Clothing
-								</h1>
-							</li>
-
-							<li className='relative group'>
-								<h1
-									onClick={() => {
-										navigate('/bag');
-										setMobileNav(false);
-									}}
-									className='underline-custom'>
-									Bags
-								</h1>
-							</li>
-
-							<li className='relative group'>
-								<h1 onClick={() => {
-										navigate('/shoes');
-										setMobileNav(false);
-									}}  className='underline-custom'>
-									Shoes
-								</h1>
-							</li>
-
-							<li className='relative flex items-end underline-custom group'>
-								<h1  className='flex items-center'>
-									Men <DownOutlined className='text-[10px]' />
-								</h1>
-								<div className='absolute z-30 hidden cursor-pointer group-hover:flex flex-col mt-0 w-[200px]  font-serrat gap-5 top-full left-0 bg-white p-4 shadow-lg transition-all duration-200'>
-									<p
-										onClick={() => {
-											navigate('/men-clothing');
-											setMobileNav(false);
-										}}>
-										Men's Clothing
-									</p>
-									<p
-										onClick={() => {
-											navigate('/men-accesories');
-											setMobileNav(false);
-										}}>
-										Men's Accessories
-									</p>
-								</div>
-							</li>
-
-							<li className='relative flex items-end underline-custom group'>
-								<h1 className='flex items-center'>
-									Women <DownOutlined className='text-[10px]' />
-								</h1>
-								<div className='absolute z-30 hidden cursor-pointer group-hover:flex flex-col mt-0 w-[200px]  font-serrat gap-5 top-full left-0 bg-white p-4 shadow-lg transition-all duration-200'>
-									<p
-										onClick={() => {
-											navigate('/women-clothing');
-											setMobileNav(false);
-										}}>
-										Women's Clothing
-									</p>
-									<p
-										onClick={() => {
-											navigate('/women-acccesories');
-											setMobileNav(false);
-										}}>
-										Women's Accessories
-									</p>
-								</div>
-							</li>
-
-							<li className='relative group'>
-								<h1 className='underline-custom'>
-									Contact us
-								</h1>
-								<div className='absolute hidden cursor-pointer group-hover:flex flex-col mt-0 w-[200px]  font-serrat gap-5 top-full left-0 bg-white p-4 shadow-lg transition-all duration-200'>
-									<p
-										onClick={() => {
-											navigate('/contact');
-											setMobileNav(false);
-										}}>
-										Contact Information
-									</p>
-									<p
-										onClick={() => {
-											navigate('/contact');
-											setMobileNav(false);
-										}}>
-										Support
-									</p>
-								</div>
+						<ul className='gap-3 text-sm flex flex-col'>
+							<li>
+								<button
+									onClick={() => toggleMenu('new')}
+									className='underline-custom w-full text-left flex justify-between items-center'>
+									What's new <DownOutlined className='text-[10px]' />
+								</button>
+								{openMenu === 'new' && (
+									<div className='flex flex-col mt-2 pl-4 gap-3 font-serrat'>
+										<p
+											onClick={() => {
+												navigate('/new-arrival');
+												setMobileNav(false);
+											}}>
+											New Arrivals
+										</p>
+										<p
+											onClick={() => {
+												navigate('/trending');
+												setMobileNav(false);
+											}}>
+											Trending
+										</p>
+									</div>
+								)}
 							</li>
 
 							<li>
-								{user ? (
-									<div className=' flex flex-col gap-5'>
-										<p onClick={() => navigate('/shop')}>View Profile</p>
+								<button
+									onClick={() => toggleMenu('shop')}
+									className='underline-custom w-full text-left flex justify-between items-center'>
+									Shop <DownOutlined className='text-[10px]' />
+								</button>
+								{openMenu === 'shop' && (
+									<div className='flex flex-col mt-2 pl-4 gap-3 font-serrat'>
 										<p
 											onClick={() => {
-												navigate('/profile');
-												logOutUser(user);
+												navigate('/shop');
 												setMobileNav(false);
 											}}>
-											Logout
+											All Products
+										</p>
+										<p
+											onClick={() => {
+												navigate('/popular-items');
+												setMobileNav(false);
+											}}>
+											Popular Items
 										</p>
 									</div>
-								) : (
-									<div className=' flex flex-col gap-5'>
-										<h1
+								)}
+							</li>
+
+							<li>
+								<button
+									onClick={() => toggleMenu('men')}
+									className='underline-custom w-full text-left flex justify-between items-center'>
+									Men <DownOutlined className='text-[10px]' />
+								</button>
+								{openMenu === 'men' && (
+									<div className='flex flex-col mt-2 pl-4 gap-3 font-serrat'>
+										<p
 											onClick={() => {
-												navigate('/login');
+												navigate('/bracelet-stacks');
 												setMobileNav(false);
 											}}>
-											{' '}
-											Create an Account
-										</h1>
-										<h2
+											Bracelet stacks
+										</p>
+										<p
 											onClick={() => {
-												navigate('/login');
+												navigate('/neck-candies');
 												setMobileNav(false);
 											}}>
-											Login
-										</h2>
+											Neck candies
+										</p>
+										<p
+											onClick={() => {
+												navigate('/grooms-owanbe-sets');
+												setMobileNav(false);
+											}}>
+											Groom’s/Owambe sets
+										</p>
+										<p
+											onClick={() => {
+												navigate('/walking-sticks');
+												setMobileNav(false);
+											}}>
+											Walking sticks
+										</p>
+									</div>
+								)}
+							</li>
+
+							<li>
+								<button
+									onClick={() => toggleMenu('women')}
+									className='underline-custom w-full text-left flex justify-between items-center'>
+									Women <DownOutlined className='text-[10px]' />
+								</button>
+								{openMenu === 'women' && (
+									<div className='flex flex-col mt-2 pl-4 gap-3 font-serrat'>
+										<p
+											onClick={() => {
+												navigate('statement-earrings');
+												setMobileNav(false);
+											}}>
+											Statement earrings
+										</p>
+										<p
+											onClick={() => {
+												navigate('beaded-bags');
+												setMobileNav(false);
+											}}>
+											Beaded bags
+										</p>
+										<p
+											onClick={() => {
+												navigate('bracelet-stacks');
+												setMobileNav(false);
+											}}>
+											Bracelet stacks
+										</p>
+										<p
+											onClick={() => {
+												navigate('bridal');
+												setMobileNav(false);
+											}}>
+											Bridal
+										</p>
+										<p
+											onClick={() => {
+												navigate('statement-throwon');
+												setMobileNav(false);
+											}}>
+											Statement throw-ons
+										</p>
+										<p
+											onClick={() => {
+												navigate('gemstone-pendant');
+												setMobileNav(false);
+											}}>
+											Gemstone pendants
+										</p>
+									</div>
+								)}
+							</li>
+
+							<li>
+								<button
+									onClick={() => toggleMenu('contact')}
+									className='underline-custom w-full text-left flex justify-between items-center'>
+									Contact us <DownOutlined className='text-[10px]' />
+								</button>
+								{openMenu === 'contact' && (
+									<div className='flex flex-col mt-2 pl-4 gap-3 font-serrat'>
+										<p
+											onClick={() => {
+												navigate('/contact');
+												setMobileNav(false);
+											}}>
+											Contact Information
+										</p>
+										<p
+											onClick={() => {
+												navigate('/contact');
+												setMobileNav(false);
+											}}>
+											Support
+										</p>
 									</div>
 								)}
 							</li>
